97f2d84fe27f8ef992c308c89e77b2555a6cf5e4
Alex Lu <alexluvg13@gmail.com>
2023-02-16 01:26:58 -0800

    adjusted images

--- src/data.js
+++ src/data.js
@@ -37,16 +37,8 @@ export const testimonials = [
   {
     quote:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
+    name: "VeriToll",
+    company: "Software Engineer / Mobile Developer",
   },
 ];
 
@@ -55,6 +47,6 @@ export const skills = [
   "Swift/SwiftUI",
   "Android Studios",
   "AWS DynamoDB",
-  "React",
+  "ReactJS",
   "C++",
 ];
